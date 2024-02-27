a93e08a6f32e6c51149c3497638e854e5fef9264
unknown <efeberktanci@gmail.com>
2024-02-28 01:31:03 +0300

    Added background image of the selected film and the name on the detail page section

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { baseImgUrl, options } from "../constant";
 
 const DetailPage = () => {
-  return (
-    <div>DetailPage</div>
-  )
-}
+  const [movie, setMovie] = useState(null);
 
-export default DetailPage
+  const { id } = useParams();
+
+  console.log(id);
+
+  useEffect(() => {
+    axios
+      .get(
+        `/movie/${id}?append_to_response=credits%2Cvideos&language=en-US`,
+        options
+      )
+      .then((res) => setMovie(res.data));
+  }, []);
+
+  return (
+    <div className="row" >
+      {!movie ? (
+        <div class="spinner-border text-primary" role="status"></div>
+      ) : (
+        <>
+          <div className="col-12 banner" >
+            <img 
+            className="w-100 h-100 object-fit-cover" 
+            src={baseImgUrl+ movie.backdrop_path} 
+            />
+            <div className="banner-bg" >
+              <span>{movie.title}</span>
+            </div>
+
+          </div>
+        </>
+      )}
+    </div>
+   
+  );
+};
+
+export default DetailPage;
